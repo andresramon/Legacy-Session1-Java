0b24fac23d26a14f2a67be28a616f41bf060696b
yeray.bajo <yeray.bajo@1YBAJOXDELL5510.babel-si.local>
2022-09-12 15:35:07 +0200

    Refactor For get Score

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -13,6 +13,13 @@ public class TennisGame1 implements TennisGame{
         put(2, "Thirty-All");
     }};
 
+    private Map<Integer, String> scoreMap = new HashMap<Integer, String>(){{
+        put(0, "Love");
+        put(1, "Fifteen");
+        put(2, "Thirty");
+        put(3, "Forty");
+    }};
+
     public TennisGame1(String player1Name, String player2Name){
         this.player1Name = player1Name;
         this.player2Name = player2Name;
@@ -28,7 +35,6 @@ public class TennisGame1 implements TennisGame{
 
     public String getScore(){
         String score = "";
-        int tempScore = 0;
         if(isEqualScore()){
             score = equalScoreMap.get(m_score1);
 
@@ -38,30 +44,15 @@ public class TennisGame1 implements TennisGame{
         }else if(isScoreOverOrEqualsFour(m_score1) || isScoreOverOrEqualsFour(m_score2)){
             score = getScoreForAdvantageOrWinnerPlayer();
         }else {
-            for(int i = 1; i < 3; i++){
-                if(i == 1)
-                    tempScore = m_score1;
-                else {
-                    score += "-";
-                    tempScore = m_score2;
-                }
-                switch(tempScore){
-                case 0:
-                    score += "Love";
-                    break;
-                case 1:
-                    score += "Fifteen";
-                    break;
-                case 2:
-                    score += "Thirty";
-                    break;
-                case 3:
-                    score += "Forty";
-                    break;
-                }
-            }
+            score = getNotEqualOrAdvantageScore();
         }
         return score;
+    }
+
+    private String getNotEqualOrAdvantageScore(){
+
+       return scoreMap.get(m_score1) + "-" + scoreMap.get(m_score2);
+
     }
 
     private String getScoreForAdvantageOrWinnerPlayer(){
